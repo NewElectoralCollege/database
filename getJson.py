b70a4e0d4d4c3d87eeee1e23106ceb29989041b7
KingWither <67877704+KingWither@users.noreply.github.com>
2021-11-15 19:33:01 -0500

    Moved getJson.py to this repository

---
+++ getJson.py
@@ -0,0 +1,26 @@
+#!C:\Python27\python
+
+import cgi
+import json
+import os
+print("Content-Type: text/html; charset=utf-8\n\n")
+
+
+year = cgi.FieldStorage().getvalue("year")
+
+output = {}
+
+directory = year + "/"
+for state in os.listdir(directory):
+    file = open(directory + state, "r")
+    state = state.split(".")[0].replace(" Of ", " of ")
+    output[state] = ""
+
+    for line in file:
+        output[state] += line.strip()
+
+    output[state] = output[state]
+
+    file.close()
+
+print(json.dumps(output))
